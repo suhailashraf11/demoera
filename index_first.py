1c39028e3427726a2dbb7ace43cd58e222dc0140
Suhail Ashraf <76818469+suhailashraf11@users.noreply.github.com>
2024-08-09 21:00:09 +0530

    Create  index_first.py
    and wrote for loop for division purpose

---
+++ index_first.py
@@ -0,0 +1,6 @@
+# This is my demo  code of python that i am writing in order to practive the work.
+print('hello world')
+print(range(10)
+for i in range(200):
+    if (i % 2 == 0) and (i % 5 == 0):
+        print(i)
